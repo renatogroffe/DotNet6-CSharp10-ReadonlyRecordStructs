c05a433ea789cdaafc8979226ade9d7a5072c571
Renato Groffe <renato.groffe@yahoo.com.br>
2022-01-22 19:14:26 -0300

    .NET 6 + Console Application + C# 10 + Readonly Record Structs
    .NET 6 + Console Application + C# 10 + Readonly Record Structs

---
+++ ExemploReadonlyRecordStructs/ReadonlyRecordStructs.cs
@@ -0,0 +1,18 @@
+namespace ExemploReadonlyRecordStructs;
+
+public readonly record struct Demografia
+{
+    public string Pais { get; init; }
+    public int Ano { get; init; }
+    public int Populacao { get; init; }
+
+    public Demografia()
+    {
+        Pais = "Brasil";
+        Ano = 2000;
+        Populacao = 174_800_000;
+    }
+    
+    public Demografia(string pais, int ano, int populacao) =>
+        (Pais, Ano, Populacao) = (pais, ano, populacao);
+}
